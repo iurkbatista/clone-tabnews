a56665f996c8e5e1ece2da3d0f6ee18dc44eb4f9
Iurk Batista <iurkbatista@gmail.com>
2025-08-22 23:03:02 -0300

    Alterando conteudo da pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Apenas um teste monkinha para ver meu server funcionando, bjo na bunda!</h1>
+    return (
+        <>
+            <h1>Apenas um teste monkinha para ver meu server funcionando, bjo na bunda!</h1>
+            <p>R: Funciona, te amo!</p>
+        </>
+    );
 }
 
 export default Home;
